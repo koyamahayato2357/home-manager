72ab0a31a4e60f79c5c6d89c85569e5683131aa6
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-08-30 18:05:32 +0900

    format

--- home.nix
+++ home.nix
@@ -155,85 +155,86 @@
       exec-once = [ "fcitx5 -d" "hyprpaper" ];
       env = [
         "XCURSOR_SIZE,24"
-	"HYPRCURSOR_SIZE,24"
+        "HYPRCURSOR_SIZE,24"
       ];
       general = {
         gaps_in = 5;
-	gaps_out = 5;
-	border_size = 2;
-	"col.active_border" = "rgba(00ffffee)";
-	"col.inactive_border" = "rgba(000000ee)";
-	resize_on_border = false;
-	allow_tearing = false;
-	layout = "dwindle";
+        gaps_out = 5;
+        border_size = 2;
+        "col.active_border" = "rgba(00ffffee)";
+        "col.inactive_border" = "rgba(000000ee)";
+        resize_on_border = false;
+        allow_tearing = false;
+        layout = "dwindle";
+        layerrule = "ignorealpha 1";
       };
       decoration = {
         rounding = 10;
-	rounding_power = 2;
-	active_opacity = 1;
-	inactive_opacity = 1;
-	blur = {
-	  enabled = true;
-          brightness = 0.5;
-	  size = 10;
-	};
+        rounding_power = 2;
+        active_opacity = 1;
+        inactive_opacity = 1;
+        blur = {
+          enabled = true;
+              brightness = 0.5;
+          size = 10;
+        };
       };
       animations = {
         enabled = "yes";
-	bezier = [
-	  "easeOutQuint,0.23,1,0.32,1"
-	  "easeInOutCubic,0.65,0.05,0.36,1"
-          "linear,0,0,1,1"
-	  "almostLinear,0.5,0.5,0.75,1"
-	  "quick,0.15,0,0.1,1"
-	];
-	animation = [
-          "global,1,10,default"
-          "border,1,5.39,easeOutQuint"
-          "windows,1,4.79,easeOutQuint"
-          "windowsIn,1,4.1,easeOutQuint,popin 87%"
-          "windowsOut,1,1.49,linear,popin 87%"
-          "fadeIn,1,1.73,almostLinear"
-          "fadeOut,1,1.46,almostLinear"
-          "fade,1,3.03,quick"
-          "layers,1,3.81,easeOutQuint"
-          "layersIn,1,4,easeOutQuint,fade"
-          "layersOut,1,1.5,linear,fade"
-          "fadeLayersIn,1,1.79,almostLinear"
-          "fadeLayersOut,1,1.39,almostLinear"
-          "workspaces,1,1.94,easeOutQuint,slide"
-          "workspacesIn,1,1.21,easeOutQuint,slide"
-          "workspacesOut,1,1.94,easeOutQuint,slide"
-	];
+        bezier = [
+          "easeOutQuint,0.23,1,0.32,1"
+          "easeInOutCubic,0.65,0.05,0.36,1"
+              "linear,0,0,1,1"
+          "almostLinear,0.5,0.5,0.75,1"
+          "quick,0.15,0,0.1,1"
+        ];
+        animation = [
+              "global,1,10,default"
+              "border,1,5.39,easeOutQuint"
+              "windows,1,4.79,easeOutQuint"
+              "windowsIn,1,4.1,easeOutQuint,popin 87%"
+              "windowsOut,1,1.49,linear,popin 87%"
+              "fadeIn,1,1.73,almostLinear"
+              "fadeOut,1,1.46,almostLinear"
+              "fade,1,3.03,quick"
+              "layers,1,3.81,easeOutQuint"
+              "layersIn,1,4,easeOutQuint,fade"
+              "layersOut,1,1.5,linear,fade"
+              "fadeLayersIn,1,1.79,almostLinear"
+              "fadeLayersOut,1,1.39,almostLinear"
+              "workspaces,1,1.94,easeOutQuint,slide"
+              "workspacesIn,1,1.21,easeOutQuint,slide"
+              "workspacesOut,1,1.94,easeOutQuint,slide"
+        ];
       };
       bind = [
         "$mod,Q,exec,$terminal"
         "$mod,B,exec,$browser"
         "$mod,E,exec,$subbrowser"
-	"$mod,C,killactive"
+        "$mod,C,killactive"
         "$mod,M,exit"
-	"$mod,1,workspace,1"
-	"$mod,2,workspace,2"
-	"$mod,3,workspace,3"
-	"$mod,4,workspace,4"
-	"$mod,5,workspace,5"
-	"$mod,6,workspace,6"
-	"$mod,7,workspace,7"
-	"$mod,8,workspace,8"
-	"$mod,9,workspace,9"
-	"$mod SHIFT,1,movetoworkspace,1"
-	"$mod SHIFT,2,movetoworkspace,2"
-	"$mod SHIFT,3,movetoworkspace,3"
-	"$mod SHIFT,4,movetoworkspace,4"
-	"$mod SHIFT,5,movetoworkspace,5"
-	"$mod SHIFT,6,movetoworkspace,6"
-	"$mod SHIFT,7,movetoworkspace,7"
-	"$mod SHIFT,8,movetoworkspace,8"
-	"$mod SHIFT,9,movetoworkspace,9"
+        "$mod,1,workspace,1"
+        "$mod,2,workspace,2"
+        "$mod,3,workspace,3"
+        "$mod,4,workspace,4"
+        "$mod,5,workspace,5"
+        "$mod,6,workspace,6"
+        "$mod,7,workspace,7"
+        "$mod,8,workspace,8"
+        "$mod,9,workspace,9"
+        "$mod SHIFT,1,movetoworkspace,1"
+        "$mod SHIFT,2,movetoworkspace,2"
+        "$mod SHIFT,3,movetoworkspace,3"
+        "$mod SHIFT,4,movetoworkspace,4"
+        "$mod SHIFT,5,movetoworkspace,5"
+        "$mod SHIFT,6,movetoworkspace,6"
+        "$mod SHIFT,7,movetoworkspace,7"
+        "$mod SHIFT,8,movetoworkspace,8"
+        "$mod SHIFT,9,movetoworkspace,9"
       ];
       windowrule = [
         "suppressevent maximize, class:.*"
-	"nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
+        "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
     };
   };
