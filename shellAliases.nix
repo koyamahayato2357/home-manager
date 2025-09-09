29c4dfa29b3f54737c7dbe5bb3b28c69046196da
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-09-09 15:04:48 +0900

    aliases

--- shellAliases.nix
+++ shellAliases.nix
@@ -1,11 +1,13 @@
 let
+  n = "nvim";
   sudo = "sudo -E";
 in {
-    c = "${sudo} nvim /etc/nixos/configuration.nix";
-    h = "nvim ~/.config/home-manager/home.nix";
+    c = "${sudo} ${n} /etc/nixos/configuration.nix";
+    h = "${n} ~/.config/home-manager/home.nix";
     hs = "home-manager switch";
     laz = "lazygit";
-    n = "nvim";
     ns = "${sudo} nixos-rebuild switch";
-    sudo = sudo;
+    rmf = "rm -rf";
+    inherit n;
+    inherit sudo;
 }
